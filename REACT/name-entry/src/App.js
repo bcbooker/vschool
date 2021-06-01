773122784879a7ea7c72bad9f0a35cce31699cc2
Brandon Booker <bcbooker@gmail.com>
2021-06-01 16:35:02 -0500

    completed REACT nameEntry

--- REACT/name-entry/src/App.js
+++ REACT/name-entry/src/App.js
@@ -1,14 +1,93 @@
-
+import React, {Component} from "react";
 import './App.css';
+import Post from "./Post"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1> Greets from app.js</h1>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super()
+
+    this.state = {
+      firstName : "",
+      lastName: "",
+      names : ["s"]
+    }
+
+    // binding this to methods
+    this.handleChange = this.handleChange.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+
+  }
+
+  // handleChange method 
+  handleChange = event =>{
+    const {name,value} = event.target
+    this.setState({[name]: value})
+    
+  }
+
+  onSubmit(event){
+    event.preventDefault(); 
+    // arr.push(event.target.children.firstName.value)
+    // console.log(event.target.children.firstName.value)
+    // console.log(arr)
+    this.setState(prevState => {
+      return {
+        names: [...this.state.names, this.state.firstName]
+      }
+      
+    })
+    console.log(event.target.input)
+    // event.target.name = "";
+    // console.log(this.state.names)
+    // console.log("submit")
+    // this.setState({ ...this.state, names: [...this.state.names, {firstName: this.state.firstName}]})   
+
+    // clears out after submit
+    this.setState({
+      firstName: ""
+    })
+  }
+  
+  render(){
+    // Mapping over names array in state and creating a new post component 
+    const namesSubmitted = this.state.names.map(name => 
+      <Post 
+      name = {name}
+      />
+      )
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1> Greets from app.js</h1>
+         
+          
+          <Post 
+          names={this.state.names}
+          />
+
+          {namesSubmitted}
+
+          <form 
+          onSubmit={this.onSubmit}>
+
+          <input
+          id="input"
+          type="text"
+          value={this.state.firstName}
+          name="firstName"
+          onChange={this.handleChange}
+          />
+
+          <br />
+      <button>Submit</button>
+      </form>
+          
+          
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
---
+++ REACT/name-entry/src/Post.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Post (props){
+    
+        
+    
+    return(
+        <div>
+            <ol>
+                <li>{props.name}</li>
+            </ol>
+            <h1></h1>
+        </div>
+    )
+}
+
+export default Post
